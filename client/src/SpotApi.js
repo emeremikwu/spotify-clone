af45a2894b942aae1f9cfaeb87dee657d0990c7d
Emmanuel <princedee3@gmail.com>
2024-04-01 13:53:47 -0500

    Reorganization

---
+++ client/src/SpotApi.js
@@ -0,0 +1,7 @@
+// import Button from 'react-bootstrap/Button';
+
+/* const client_id = '3cba2013d88445e998cec4dbefb580e6';
+const client_secret = 'ef08cd27f81744e7ba7cfc5130654625'; */
+
+// eslint-disable-next-line no-unused-vars
+const API_ENDPOINT = 'https://accounts.spotify.com/api/token';
